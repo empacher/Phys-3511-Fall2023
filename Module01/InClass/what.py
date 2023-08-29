1b87b4174e2b8532b4f21d804a42421957e2b6fe
empacher <97556067+empacher@users.noreply.github.com>
2023-08-29 19:38:10 -0400

    Add files via upload

---
+++ Module01/InClass/what.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+list1 = ['wh','Y','gon','ca']
+list2 = ['o','a','na','ll']
+
+list3 = [i + j for i, j in zip(list1, list2)]
+print(list3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
